545e8bab5ef96dbc631735796d980040a78b3ba9
Reyhan Sena Çimen <reyhansena42@gmail.com>
2026-04-19 17:32:23 +0300

    player move deneme

---
+++ Assets/Components/Movement/PlayerMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMove : Move
+{
+    void Start()
+    {
+        Initialize(transform, 5f);
+    }
+
+    void Update()
+    {
+        Tick();
+    }
+
+    public override void Tick()
+    {
+        if (!CanMove()) return;
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0f).normalized;
+
+        _transform.position += direction * _speed * Time.deltaTime;
+    }
+}
+
